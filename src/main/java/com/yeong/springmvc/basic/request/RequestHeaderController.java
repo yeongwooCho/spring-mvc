c65aaa36eb110f3f043c642403e4105c6177a23d
yeongwoo <jyy0223@naver.com>
2024-06-25 17:49:28 +0900

    [feat] HTTP request header 조회

---
+++ src/main/java/com/yeong/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,38 @@
+package com.yeong.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale, // 언어 정보
+                          @RequestHeader MultiValueMap<String, String> headerMap, // 헤더 전부
+                          @RequestHeader("host") String host, // 헤더 1개
+                          @CookieValue(value = "myCookie", required = false) String cookie // 필수값 아님
+    ) {
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+
+        return "ok";
+    }
+}
