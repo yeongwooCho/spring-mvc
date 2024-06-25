786c3635e105479791a6086867686489c07a7e68
yeongwoo <jyy0223@naver.com>
2024-06-25 21:38:06 +0900

    [feat] Thymeleaf view templates 적용

---
+++ src/main/java/com/yeong/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package com.yeong.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        return new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2() {
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+
+}
